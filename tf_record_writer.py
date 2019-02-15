de9f17a1ac83e306bc119c5c4e661a61cf914787
ferrotem <32492151+ferrotem@users.noreply.github.com>
2019-02-15 14:21:39 +0100

    Update tf_record_writer.py

--- tf_record_writer.py
+++ tf_record_writer.py
@@ -8,7 +8,7 @@ from dataset_manager import Dataset
 import config
 
 
-class tf_writer:
+class tf_record_writer:
     def __init__(self):
 
         os.environ['TF_CPP_MIN_LOG_LEVEL'] = config.TF_CPP_MIN_LOG_LEVEL
@@ -170,4 +170,4 @@ class tf_writer:
                 tfwriter.write(example.SerializeToString())
 
 
-tf_writer()
+tf_record_writer()
